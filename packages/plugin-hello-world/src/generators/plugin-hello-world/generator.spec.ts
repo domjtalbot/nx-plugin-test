76c84817632573b76ca31c02ce302f4f0b0239c1
Dom <3301714+domjtalbot@users.noreply.github.com>
2022-06-03 10:11:40 +0100

    create `plugin-hello-world`

---
+++ packages/plugin-hello-world/src/generators/plugin-hello-world/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { PluginHelloWorldGeneratorSchema } from './schema';
+
+describe('plugin-hello-world generator', () => {
+  let appTree: Tree;
+  const options: PluginHelloWorldGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
---
+++ packages/plugin-hello-world/src/generators/plugin-hello-world/schema.d.ts
@@ -0,0 +1,5 @@
+export interface PluginHelloWorldGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}

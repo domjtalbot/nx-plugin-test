e54d06578e4f7799bb7073070f832f2d5a79b677
Dom <3301714+domjtalbot@users.noreply.github.com>
2022-06-03 10:37:16 +0100

    add logger

--- libs/plugin-hello-world/src/executors/build/executor.ts
+++ libs/plugin-hello-world/src/executors/build/executor.ts
@@ -1,7 +1,12 @@
+import { logger } from '@nrwl/devkit';
+
 import { BuildExecutorSchema } from './schema';
 
 export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for Build', options);
+  logger.debug('NX Plugin Hello World', options);
+  
+  logger.warn('Hello World!');
+
   return {
     success: true,
   };
